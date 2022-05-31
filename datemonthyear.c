64f91b28dfaeba5ff48f399ec1d1c2f8d9f61ac2
Dilipkumar <101540362+kotinidilip@users.noreply.github.com>
2022-05-31 17:57:21 +0000

    nothing

---
+++ datemonthyear.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int dd, mm, yy;
+    int date;
+
+    printf("Enter date (dd/mm/yy) format: ");
+    scanf("%d/%d/%d", &dd, &mm, &yy);
+
+    printf("\nEntered date is: %02d/%02d/%04d\n", dd, mm, yy);
+
+  
+    date = 0;
+    date |= (dd & 0xff); 
+    date |= (mm & 0xff) << 8; 
+    date |= (yy & 0xffff) << 16; 
+
+    printf("Date in single variable: %d [Hex: %08X] \n", date, date);
+
+   
+
+    dd = (date & 0xff); 
+    mm = ((date >> 8) & 0xff);
+    yy = ((date >> 16) & 0xffff); 
+
+    printf("Date after extracting: %02d/%02d/%04d\n", dd, mm, yy);
+
+    return 0;
+}
+Output:
+Entered date is: 22/04/2003
+Date in single variable: 131269654 [Hex: 07D30416] 
+Date after extracting: 22/04/2003
